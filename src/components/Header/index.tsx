e8aa7ce02a2aea749e77cbe216b47a8ca61dd719
VanessaMaia26 <vanessamaia2607@gmail.com>
2022-12-05 12:05:16 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer, HeaderContent, NewTransactionBuutton } from "./styles";
+import logoImg from '../../assets/logo.svg';
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} alt="" />
+
+                <Dialog.Root>
+                  <Dialog.Trigger asChild>
+                    <NewTransactionBuutton>Nova Transição</NewTransactionBuutton>
+                  </Dialog.Trigger>
+
+                  <NewTransactionModal />
+                </Dialog.Root>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
